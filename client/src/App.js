e34740f1c07fd5495e3f4843e3dbb6a57039cb5a
sanjay s <sanman014@gmail.com>
2021-07-20 09:46:16 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
+import Dashboard from "./components/dashboard/Dashboard";
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -30,7 +34,10 @@ const App = () => {
             <Alert />
             <Switch>
               <Route exact path="/register" component={Register} />
+              <Route exact path="/create-profile" component={CreateProfile} />
+              <Route exact path="/edit-profile" component={EditProfile} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
