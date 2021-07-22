2df57e38a6ad38756b8bb11928f7234c2d5c69ad
sanjay s <sanman014@gmail.com>
2021-07-22 11:18:14 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Profile from "./components/profile/Profile";
 import Profiles from "./components/profiles/Profiles";
 import Posts from "./components/posts/Posts";
 import Post from "./components/post/Post";
+import NotFound from "./components/layout/NotFound";
 import Dashboard from "./components/dashboard/Dashboard";
 //Redux
 import { Provider } from "react-redux";
@@ -66,6 +67,7 @@ const App = () => {
               />
               <PrivateRoute exact path="/posts" component={Posts} />
               <PrivateRoute exact path="/posts/:id" component={Post} />
+              <Route component={NotFound} />
             </Switch>
           </section>
         </Fragment>
